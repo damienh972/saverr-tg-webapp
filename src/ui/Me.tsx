03b00be9042a8f5fbc2cce888ef295fe85980ac8
damienh972 <helo.dams@gmail.com>
2026-01-03 13:10:13 +0100

    fix: typo and transfer UX

--- src/ui/Me.tsx
+++ src/ui/Me.tsx
@@ -96,7 +96,7 @@ export default function Me() {
               className="menu-text"
               style={{ color: "#1b2224", fontFamily: "Roboto", fontSize: "16px" }}
             >
-              +{data.user.phone}
+              {data.user.phone}
             </div>
           </div>
         </div>
